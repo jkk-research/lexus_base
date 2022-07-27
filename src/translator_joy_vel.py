0e35c88b55ad7509e2eb4fc3ef5c4b16b60f17af
horverno <horverno@gmail.com>
2022-07-27 16:27:52 +0200

    Added game wheel first version

---
+++ src/translator_joy_vel.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# cmd_vel to /pacmod/as_rx/accel_cmd brake and steer
+
+import rospy
+import pacmod_msgs.msg as pac_msg
+from sensor_msgs.msg import Joy
+
+class Translator:
+    def __init__(self):
+        self.sub = rospy.Subscriber("joy", Joy, self.callback)
+        self.pubA = rospy.Publisher("/pacmod/as_rx/accel_cmd", pac_msg.SystemCmdFloat, queue_size=1)
+        self.pubB = rospy.Publisher("/pacmod/as_rx/brake_cmd", pac_msg.SystemCmdFloat, queue_size=1)
+        self.pubS = rospy.Publisher("/pacmod/as_rx/steer_cmd", pac_msg.SteerSystemCmd, queue_size=1)
+        self.last_published_time = rospy.get_rostime()
+        self.last_published = None
+        self.timer = rospy.Timer(rospy.Duration(1./20.), self.timer_callback)
+        self.firstRun = True
+        
+    def timer_callback(self, event):
+        if self.last_published and self.last_published_time < rospy.get_rostime() + rospy.Duration(1.0/20.):
+            self.callback(self.last_published)
+
+    def callback(self, message):
+        accelCmd = pac_msg.SystemCmdFloat()
+        brakeCmd = pac_msg.SystemCmdFloat()
+        steerCmd = pac_msg.SteerSystemCmd()
+        accelCmd.header.stamp = rospy.Time.now()
+        brakeCmd.header.stamp = rospy.Time.now()
+        steerCmd.header.stamp = rospy.Time.now()
+        if (self.firstRun):
+            accelCmd.clear_override = True
+            self.firstRun = False
+        else:
+            accelCmd.clear_override = False
+        """
+        if message.axes.x > 0.2:
+            accelCmd.command = message.linear.x
+            brakeCmd.command = 0.0
+            accelCmd.enable = False
+        elif message.linear.x < -0.1:
+            accelCmd.command = 0.0
+            brakeCmd.command = -1 * message.linear.x
+            accelCmd.enable = True
+        else:
+            accelCmd.command = 0.0
+            brakeCmd.command = 0.0
+            accelCmd.enable = True
+        """
+        steerCmd.command = message.axes[0] * 6
+        accelCmd.command = (message.axes[1] + 1) 
+        brakeCmd.command = (message.axes[2] + 1) 
+        steerCmd.rotation_rate = 3.3
+        accelCmd.header.frame_id = "pacmod"        
+        brakeCmd.enable = True
+        accelCmd.enable = True
+        steerCmd.enable = True
+        rospy.loginfo("accel brake steer: %.1f %.1f %.1f" %(accelCmd.command, brakeCmd.command, steerCmd.command))
+        self.last_published = message
+        self.pubA.publish(accelCmd)
+        self.pubB.publish(brakeCmd)
+        self.pubS.publish(steerCmd)
+
+if __name__ == '__main__':
+    rospy.init_node("translator_cmd_vel")
+    rospy.loginfo("translator_cmd_vel started")
+    t = Translator()
+    rospy.spin()
