a7911d2b15268d215116149a6785d50d51cf2ecf
horverno <horverno@gmail.com>
2022-10-20 14:48:23 +0200

    TF update

---
+++ etc/rosbag_important.sh
@@ -0,0 +1,11 @@
+cd ~/bag/
+mkdir -p $(date -I)
+cd $(date -I)
+TEXT1="$1"
+TIME1="$(date +"%Y-%m-%d_%H-%M")"
+FILE1="$TEXT1$TIME1.bag"
+FILE2="x_rosparam_dump_$TEXT1$TIME1.txt"
+PWD1="$(pwd)"
+echo "Writing to file: $PWD1/$FILE1"
+#rosparam dump $FILE2
+rosbag record -O $FILE1 -b 4096 /gps/duro/current_pose /gps/duro/imu /gps/duro/status_flag /gps/duro/status_string /os_center/points /os_right/points /zed_node/left/camera_info /zed_node/left/image_rect_color
