9515f278caadd58a803f72d23caca555c5d157b6
horverno <horverno@gmail.com>
2022-10-24 14:41:37 +0200

    Update before static demo

--- etc/demo_static.sh
+++ etc/demo_static.sh
@@ -14,8 +14,12 @@ echo "[INFO] Starting zed_default"
 screen -d -m -S driver_zed2_def bash -c 'roslaunch lexus_base zed_default.launch'
 echo "[INFO] Starting gps_duro"
 screen -d -m -S driver_duro_gps bash -c 'roslaunch lexus_base gps_duro.launch'
-echo "[INFO] Starting tf_static"
+screen -d -m -S driver_os64cent bash -c 'roslaunch lexus_base os_64_center_a.launch'
+screen -d -m -S driver_os32righ bash -c 'roslaunch lexus_base os_32_right_a.launch'
+echo "[INFO] Starting tf"
 screen -d -m -S tf_static bash -c 'roslaunch lexus_base tf_static.launch'
+screen -d -m -S tf_duro_g bash -c 'roslaunch lexus_base tf_gps_duro.launch'
+screen -d -m -S can_pacmo bash -c 'roslaunch lexus_base can_pacmod_a.launch'
 echo "[INFO] Starting 3D"
 screen -d -m -S 3d bash -c 'roslaunch lexus_base 3d.launch'
 echo "[INFO] Starting rviz"
--- etc/rosbag_important.sh
+++ etc/rosbag_important.sh
@@ -8,4 +8,4 @@ FILE2="x_rosparam_dump_$TEXT1$TIME1.txt"
 PWD1="$(pwd)"
 echo "Writing to file: $PWD1/$FILE1"
 #rosparam dump $FILE2
-rosbag record -O $FILE1 -b 4096 /gps/duro/current_pose /gps/duro/imu /gps/duro/status_flag /gps/duro/status_string /os_center/points /os_right/points /zed_node/left/camera_info /zed_node/left/image_rect_color
+rosbag record -O $FILE1 -b 4096 /tf /gps/duro/current_pose /gps/duro/imu /gps/duro/status_flag /gps/duro/status_string /os_center/points /os_right/points /zed_node/left/camera_info /zed_node/left/image_rect_color /pacmod/parsed_tx/accel_rpt /pacmod/parsed_tx/brake_rpt /pacmod/parsed_tx/steer_rpt
