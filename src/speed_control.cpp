606d0dfcae1171c273bb913166ddbd7fc930ef42
szepilot <szepilot@gmail.com>
2023-02-22 13:07:56 +0100

    Before PID test

--- src/speed_control.cpp
+++ src/speed_control.cpp
@@ -8,18 +8,31 @@
 #include "pacmod_msgs/SystemRptFloat.h"
 #include "pacmod_msgs/SteerSystemCmd.h"
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
 
 
 ros::Publisher accel_pub;
 ros::Publisher brake_pub;
 ros::Publisher steer_pub;
-double vehicle_speed_actual;
-double vehicle_speed_reference;
-double vehicle_steering_reference;
-double speed_diff, speed_diff_kmh;
+ros::Publisher enable_pub;
+ros::Publisher status_string_pub;
+double vehicle_speed_actual, vehicle_speed_reference, vehicle_steering_reference;
+double speed_diff, speed_diff_kmh, speed_diff_prev;
+ros::Time current_time, prev_time;
+double dt = 0.033333; // /lexus3/pacmod/parsed_tx/vehicle_speed_rpt is assumed 30 Hz
+double p_out_accel, i_out_accel, d_out_accel = 0.0;
+double p_out_brake, i_out_brake, d_out_brake = 0.0;
+double const _max_i_accel = 1.0, _max_i_brake = 1.0; // anti windup constants TODO: check valid params 
+double t_integ_accel, t_integ_brake; // anti windup
+double t_derivative_accel, t_derivative_brake;
+bool autonom_status_changed = true;
+bool first_run = true, standstill = false;
+std_msgs::String status_string_msg;
 pacmod_msgs::SystemCmdFloat accel_command;
 pacmod_msgs::SystemCmdFloat brake_command;
 pacmod_msgs::SteerSystemCmd steer_command;
+std_msgs::Bool enable_msg;
 my_dyn_rec::MyParamsConfig pid;
 
 // Functions
@@ -42,6 +55,8 @@ int main(int argc, char **argv)
     accel_pub = n.advertise<pacmod_msgs::SystemCmdFloat>("/pacmod/as_rx/accel_cmd", 1);
     brake_pub = n.advertise<pacmod_msgs::SystemCmdFloat>("/pacmod/as_rx/brake_cmd", 1);
     steer_pub = n.advertise<pacmod_msgs::SteerSystemCmd>("/pacmod/as_rx/steer_cmd", 1);
+    enable_pub = n.advertise<std_msgs::Bool>("/pacmod/as_rx/enable", 1);
+    status_string_pub = n.advertise<std_msgs::String>("control_status", 1);
     ros::spin();
     return 0;
 }
@@ -49,6 +64,7 @@ int main(int argc, char **argv)
 // Callback for speed messages
 void speedCurrentCallback(const pacmod_msgs::VehicleSpeedRpt &speed_msg)
 {
+    current_time = ros::Time::now();
     vehicle_speed_actual = speed_msg.vehicle_speed;
     steer_command.command = vehicle_steering_reference;
     //ROS_INFO_STREAM("speed: " << speed_msg.vehicle_speed);
@@ -57,22 +73,77 @@ void speedCurrentCallback(const pacmod_msgs::VehicleSpeedRpt &speed_msg)
     //ROS_INFO_STREAM(" diff km/h: " << speed_diff_kmh);
     if(speed_diff > 0){
         //ROS_INFO_STREAM("accelerate");
-        accel_command.command = speed_diff * pid.p_gain_accel;
+        p_out_accel = speed_diff * pid.p_gain_accel * dt;
+        t_integ_accel += p_out_accel * dt;
+        // Restrict to max Anti-Windup
+        if( t_integ_accel > _max_i_accel)
+            t_integ_accel = _max_i_accel;
+        i_out_accel = t_integ_accel * pid.i_gain_accel;
+        t_derivative_accel = (speed_diff - speed_diff_prev) / dt;
+        d_out_accel = pid.d_gain_accel * t_derivative_accel;
+        accel_command.command = p_out_accel + i_out_accel + d_out_accel;
         brake_command.command = 0.0;
     }
     else if(speed_diff < 0){
         //ROS_INFO_STREAM("brake");
-        brake_command.command = -1.0 * speed_diff * pid.p_gain_brake;
+        p_out_brake = speed_diff * pid.p_gain_brake * dt;
+        t_integ_brake += p_out_brake * dt;
+        // Restrict to max Anti-Windup
+        if( t_integ_brake > _max_i_brake)
+            t_integ_brake = _max_i_brake;
+        i_out_brake = t_integ_brake * pid.i_gain_brake;
+        t_derivative_brake = (speed_diff - speed_diff_prev) / dt;
+        d_out_brake = pid.d_gain_brake * t_derivative_brake;
+        brake_command.command = -1.0 * (p_out_brake + i_out_brake + d_out_brake);
         accel_command.command = 0.0;
     }
+    // TODO: add standstill ?
+    if (standstill){
+        brake_command.command = 0.2;
+        accel_command.command = 0.0;
+        status_string_msg.data = "standstill";
+    }
+    if(autonom_status_changed){ 
+        accel_command.clear_override = true;
+        brake_command.clear_override = true;
+        steer_command.clear_override = true;
+        autonom_status_changed = false; // just once
+    }
+    else{
+        accel_command.clear_override = false;
+        brake_command.clear_override = false;
+        steer_command.clear_override = false;
+    }    
+    if(!first_run){
+        dt = (current_time - prev_time).toSec();
+        status_string_msg.data = "lateral_control_started";
+        //ROS_INFO_STREAM("dt: " << dt);
+    }
+    {
+        // /lexus3/pacmod/parsed_tx/vehicle_speed_rpt is assumed 30 Hz
+        dt = 0.033333;
+    }
+    if(first_run){
+        first_run = false;
+        status_string_msg.data = "longitudinal_control_started";
+    }
+
+
     steer_command.rotation_rate = 3.3;
     accel_command.header.frame_id = "pacmod";
     accel_command.enable = true;
     brake_command.enable = true;
     steer_command.enable = true;
+    enable_msg.data = true;
+    enable_pub.publish(enable_msg);
     accel_pub.publish(accel_command);
     brake_pub.publish(brake_command);
     steer_pub.publish(steer_command);
+    if(status_string_msg.data.compare(""))
+        status_string_pub.publish(status_string_msg);
+    status_string_msg.data = "";
+    prev_time = current_time; 
+    speed_diff_prev = speed_diff;
     
 }
 
